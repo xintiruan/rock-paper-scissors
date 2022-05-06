705038888cb22bcebebe5a53ea19e0ab762a1c76
xintiruan <xintiruan@hotmail.com>
2022-05-06 13:39:48 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+let win = 0
+input.onGesture(Gesture.Shake, function () {
+    win = randint(0, 2)
+    if (win == 0) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (win == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
